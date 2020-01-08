cb99a10526f90702160f070ccebfe05f42c9833d
sylvance_theone <kerandisylvance@gmail.com>
2020-01-08 16:21:02 +0300

    Update MultiplesOf3or5SolutionBestSln.cs

--- MultiplesOf3or5SolutionBestSln.cs
+++ MultiplesOf3or5SolutionBestSln.cs
@@ -2,7 +2,7 @@ using System.Linq;
 
 public static class Kata
 {
-  public static int Solution(int value)
+  public static int MultiplesOf3or5Solution(int value)
   {
     return Enumerable.Range(0, value)
                      .Where(x => x % 3 == 0 || x % 5 == 0)
